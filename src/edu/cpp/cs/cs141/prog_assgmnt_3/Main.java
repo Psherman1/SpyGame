85d2a1131dfb829b3e2aa87a74495910120a3f05
Psh <Psherman1@users.noreply.github.com>
2017-10-25 10:13:16 -0700

    Initial commit

---
+++ src/edu/cpp/cs/cs141/prog_assgmnt_3/Main.java
@@ -0,0 +1,28 @@
+/**
+ * CS 141: Intro to Programming and Problem Solving
+ * Professor: Edwin Rodr&iacute;guez
+ *
+ * Programming Assignment #N
+ *
+ * <description-of-assignment>
+ *
+ *	 Team Members:
+ *   Nick Huiting
+ *   
+ */
+package edu.cpp.cs.cs141.prog_assgmnt_3;
+
+/**
+ * @author Nick Huiting
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
