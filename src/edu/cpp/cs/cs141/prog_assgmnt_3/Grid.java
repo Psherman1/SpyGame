5946c1347dc8ee1d93a02fe3494afca090924cb9
teddysm <doan.teddy@gmail.com>
2017-10-30 21:39:10 -0700

    More classes
    classes

---
+++ src/edu/cpp/cs/cs141/prog_assgmnt_3/Grid.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package edu.cpp.cs.cs141.prog_assgmnt_3;
+
+/**
+ * @author Thanh Doan
+ *
+ */
+public class Grid {
+
+}
---
+++ src/edu/cpp/cs/cs141/prog_assgmnt_3/Gun.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package edu.cpp.cs.cs141.prog_assgmnt_3;
+
+/**
+ * @author Thanh Doan
+ *
+ */
+public class Gun {
+	private int ammo;
+
+}
