9cdc06ad87c6dea3f18b07ec1b725d65de952a0a
Jose Rodriguez <32783362+jmrodriguez4@users.noreply.github.com>
2017-11-20 16:23:01 -0800

    Started commenting exception file
    too dumb to understand for now

--- src/edu/cpp/cs/cs141/prog_assgmnt_3/Exceptions/GameStateException.java
+++ src/edu/cpp/cs/cs141/prog_assgmnt_3/Exceptions/GameStateException.java
@@ -14,7 +14,7 @@ import edu.cpp.cs.cs141.prog_assgmnt_3.Engine.GameState;
 
 /**
  * @author Nick Huiting
- *
+ * Class to handle game state exceptions.
  */
 public class GameStateException extends Exception {
 	
@@ -22,12 +22,25 @@ public class GameStateException extends Exception {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * 
+	 */
 	private GameState state;
+	
+	/**
+	 * 
+	 * @param message
+	 * @param state
+	 */
 	public GameStateException(String message, GameState state) {
 		super(message);
 		this.state = state;
 	}
 	
+	/**
+	 * 
+	 */
 	@Override
 	public String toString() {
 		String message = super.toString(); 
--- src/edu/cpp/cs/cs141/prog_assgmnt_3/Exceptions/PositionException.java
+++ src/edu/cpp/cs/cs141/prog_assgmnt_3/Exceptions/PositionException.java
@@ -19,20 +19,33 @@ package edu.cpp.cs.cs141.prog_assgmnt_3.Exceptions;
 import edu.cpp.cs.cs141.prog_assgmnt_3.Position;
 
 /**
- *
+ * @author Nick Huiting
+ * Class for catching the exceptions of the positions of objects on the grid.
  */
 public class PositionException extends Exception {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * 
+	 */
 	private final Position position;
 	
+	/**
+	 * 
+	 * @param message
+	 * @param position
+	 */
 	public PositionException(String message, Position position) {
 		super(message);
 		this.position = position;
 	}
 	
+	/**
+	 * 
+	 */
 	@Override
 	public String toString() {
 		String message = super.toString(); 
